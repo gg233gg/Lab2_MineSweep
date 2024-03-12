a126ab8362bb5204fbc91add4e759dc5cbe77318
gg233 <week1016@163.com>
2024-03-12 20:33:31 +0800

    增加空白展开功能

--- src/Mine.java
+++ src/Mine.java
@@ -2,27 +2,9 @@ import java.util.Random;
 import java.util.Scanner;
 public class Mine {
 
-    public static void main(String[] args) {
-        System.out.println("欢迎来到钱玉航设计的扫雷游戏，请输入三个整数");
-        Mine mine = new Mine();
-        boolean flag = true;
-
-        while(flag) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                mine = new Mine(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
-                flag = false;
-            } catch (Exception ex) {
-                System.out.println("invalid input");
-            }
-        }
-        mine.printField();
-
-    }
-
-
     //-1代表地雷，0-8代表周围地雷数量，-2作为下标越界的返回值
     private int[][] field;
+    private boolean[][] show;
     private final int row, col;
     private final int mines;
     private int mines_found;
@@ -40,6 +22,12 @@ public class Mine {
         col = vcol;
         mines = vmines;
         field = new int[row][col];
+        show = new boolean[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                show[i][j] = false;
+            }
+        }
         placeMines();
         placeNum();
     }
@@ -47,7 +35,7 @@ public class Mine {
     //随机放地雷
     private void placeMines() {
         /*建立一个大小为row*col的数组，其中填满从0-row*col的数字，之后使用Fisher-Yates算法打乱，
-        取出mines个数字最为地雷坐标，具体坐标为(num/row,num%row)                         */
+        取出mines个数字作为地雷坐标，其坐标为(num/row,num%row)                         */
         int size = row * col;
         int[] arr = new int[size];
         for (int i = 0; i < size; i++)
@@ -89,8 +77,8 @@ public class Mine {
     public void printField() {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (readField(i, j) == -1) System.out.print("■  ");
-                else System.out.print(readField(i, j) + "  ");
+                if (isShow(i, j)) System.out.print(readField(i, j) + "  ");
+                else System.out.print("■  ");
             }
             System.out.print("\n");
         }
@@ -100,11 +88,49 @@ public class Mine {
         field[x][y] = status;
     }
 
+    //读取某个位置的数值，并进行边界判断
     public int readField(int x, int y) {
         if (x < 0 || y < 0 || x > row - 1 || y > col - 1)
             return -2;
         else
             return field[x][y];
+    }
+
+    public boolean isMine(int x, int y) {
+        if (readField(x, y) == -1) return true;
+        else return false;
+    }
+
+    public boolean isShow(int x, int y) {
+        return show[x][y];
+    }
+
+    public void flip(int x, int y) {
+        show[x][y] = !show[x][y];
+    }
+
+    public boolean digMine(int x, int y) {
+        if (isMine(x, y)) return true;
+        else {
+            expand(x, y);
+            return false;
+        }
+    }
+
+    //揭开当前的格点空白展开，在此之前不要先将隐藏的格点揭开
+    public void expand(int x, int y) {
+        if (isMine(x, y) || readField(x, y) == -2 || isShow(x,y)) return;
+        else{
+            flip(x,y);
+            int count = countMines(x, y);
+            if (count == 0) {
+                for (int i = x - 1; i <= x + 1; i++) {
+                    for (int j = y - 1; j <= y + 1; j++) {
+                        expand(i, j);
+                    }
+                }
+            }
+        }
     }
 
 }
